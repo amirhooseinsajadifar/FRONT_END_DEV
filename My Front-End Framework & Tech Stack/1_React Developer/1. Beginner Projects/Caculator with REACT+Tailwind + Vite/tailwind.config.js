c76e4c6c6953644c5850bee60727d37e190d9878
amirhoosein-sajadifar <sajadifaramirhoosein@gmail.com>
2025-07-30 12:21:07 +0400

    Delete My Front-End Framework & Tech Stack/1_React Developer directory

--- My Front-End Framework & Tech Stack/1_React Developer/1. Beginner Projects/Caculator with REACT+Tailwind + Vite/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  darkMode: "class", // enable dark mode
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
